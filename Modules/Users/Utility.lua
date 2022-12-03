664066a677f2e0e71dc7581fef49187b29b69379
frackz <65610875+frackz@users.noreply.github.com>
2022-12-03 23:37:38 +0100

    changes

--- Modules/Users/Utility.lua
+++ Modules/Users/Utility.lua
@@ -1,12 +1,16 @@
-Users.Utility.GetSteam = function(source)
-    local steam = ''
-    if not source then return steam end
+Users.Utility.GetIdentifier = function(source, id)
+    local identifier = ''
+    if not source then return '' end
     local identifiers = GetPlayerIdentifiers(source)
-    for _, v in pairs(identifiers) do
-        if string.find(v, "steam") then
-            steam = v:gsub("steam:", "")
+    for _v in pairs(identifiers) do
+        if string.find(v, 'id') then
+            identifier = v:gsub(id+':', "")
             break
         end
     end
-    return steam
+    return identifier
+end
+
+Users.Utility.GetSteam = function(source)
+    return Users.Utility.GetIdentifier(source, 'steam')
 end
